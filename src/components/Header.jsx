ed30c81ef6a892b53c505a7829b771f80cfba484
Teo Jia En <jiaenteo.01@gmail.com>
2025-08-14 09:19:26 +0800

    Make first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
